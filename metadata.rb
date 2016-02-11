fc763a1729c3232aad174a719f335aedc89e7581
Peter Zhao <peter.qs.zhao@gmail.com>
2016-02-11 08:27:42 -0500

    PZ add gocd_cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'go'
+maintainer       'Peter Zhao'
+maintainer_email ''
+license          'All rights reserved'
+description      'Installs/Configures go'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,55 @@
+cookbook_file '/etc/yum.repos.d/thoughtworks-go.repo' do
+  source 'thoughtworks-go.repo'
+end
+
+
+group 'go'
+
+user 'go' do
+  supports :manage_home => true
+  gid 'go'
+end
+
+
+{ 'java-1.7.0-openjdk-devel' => '1.7.0.85-2.6.1.3.el6_6',
+  'go-server' => '15.2.0-2248', 
+  'go-agent' => '15.2.0-2248' }.each do |package_name, version|
+  package package_name do
+    version version
+  end
+end
+
+
+['go-server', 'go-agent'].each do |service_name|
+  service service_name do
+    action [:start, :enable]
+  end
+
+  template "/etc/default/#{service_name}" do
+    source "#{service_name}.erb" 
+    owner 'go'
+    group 'go'
+    notifies :restart, "service[#{service_name}]"
+  end
+end
+
+template '/etc/go/cruise-config.xml' do
+  source 'cruise-config.xml.erb'
+  owner 'go'
+  group 'go'
+  notifies :restart, "service[go-server]"
+  action :create_if_missing
+end
+
+directory '/var/lib/go-agent/config' do
+  owner 'go'
+  group 'go'
+end
+
+template "/var/lib/go-agent/config/autoregister.properties" do
+  source 'autoregister.properties.erb'
+  mode '0644'
+  group 'go'
+  owner 'go'
+  notifies :restart, "service[go-agent]"
+end
