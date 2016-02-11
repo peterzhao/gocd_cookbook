7598d6f74137795eeb255c4e9c9afee29172afeb
Peter Zhao <peter.qs.zhao@gmail.com>
2016-02-11 08:35:18 -0500

    PZ do not autoregister by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['java']['home'] = '/usr/lib/jvm/jre-1.7.0-openjdk.x86_64'
 default['go']['autoregister_key'] = '388b633a88de126531afa41eff9aa69e'
+default['go']['autoregister'] = false
--- recipes/default.rb
+++ recipes/default.rb
@@ -33,23 +33,26 @@ end
   end
 end
 
-template '/etc/go/cruise-config.xml' do
-  source 'cruise-config.xml.erb'
-  owner 'go'
-  group 'go'
-  notifies :restart, "service[go-server]"
-  action :create_if_missing
-end
+if node['go']['autoregister'] == true 
+    # then the pipeline config is taken over by chef. Any manual chnangs will be lost
+  template '/etc/go/cruise-config.xml' do
+    source 'cruise-config.xml.erb'
+    owner 'go'
+    group 'go'
+    notifies :restart, "service[go-server]"
+    action :create 
+  end
 
-directory '/var/lib/go-agent/config' do
-  owner 'go'
-  group 'go'
-end
+  directory '/var/lib/go-agent/config' do
+    owner 'go'
+    group 'go'
+  end
 
-template "/var/lib/go-agent/config/autoregister.properties" do
-  source 'autoregister.properties.erb'
-  mode '0644'
-  group 'go'
-  owner 'go'
-  notifies :restart, "service[go-agent]"
+  template "/var/lib/go-agent/config/autoregister.properties" do
+    source 'autoregister.properties.erb'
+    mode '0644'
+    group 'go'
+    owner 'go'
+    notifies :restart, "service[go-agent]"
+  end
 end
